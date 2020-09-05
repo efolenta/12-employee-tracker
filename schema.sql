a6fa071413b3e569344d41aa60b08ebdf12d5259
Eric Folenta <ericfolenta@gmail.com>
2020-09-05 13:36:18 -0700

    updated schema for flawless execution

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
 -- Create the Department Table --
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
