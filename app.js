900d5fe43446ffcaeb8427daf594dc290512ea94
Eric Folenta <ericfolenta@gmail.com>
2020-09-12 17:33:31 -0700

    updated functions

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ connection.connect(function(err) {
   start();
 });
 
+
+// This is the initializer. When the application is started for the first time, this function will run which is the menu interface for the application 
 function start() {
   inquirer.prompt({
     name: "home",
@@ -50,9 +52,103 @@ function start() {
     else if (answer.home === "View All Roles") {
       viewRoles();
     }
+    else if (answer.home === "Add Employee") {
+      addEmployee();
+    }
+    else if (answer.home === "Add Department") {
+      addDepartment();
+    }
+    else if (answer.home === "Add Role") {
+      addRole();
+    }
+    else if (answer.home === "Remove Employee") {
+      removeEmployee();
+    }
+    else if (answer.home === "Remove Department") {
+      removeDepartment();
+    }
+    else if (answer.home === "Remove Role") {
+      removeRole();
+    }
     else {
       console.log("Thank you for using the Employee Manager. Goodbye!");
       connection.end();
     }
+  });
+}
+// This function is used to view all employees in the database.
+function viewEmployees() {
+  connection.query("SELECT * FROM employee", function(error, results) {
+    if (error) throw error;
+    console.table(results);
+    start();
+  })
+}
+
+// This function is used to view all departments in the database.
+function viewDepartments() {
+  connection.query("SELECT * FROM department", function(error, results) {
+    if (error) throw error;
+    console.table(results);
+    start();
+  })
+}
+
+// This function is used to view all roles in the database.
+function viewRoles() {
+  connection.query("SELECT * FROM role", function(error, results) {
+    if (error) throw error;
+    console.table(results);
+    start();
+  })
+}
+
+// This function is used to create new departments in the database.
+function addDepartment() {
+  inquirer.prompt([
+    {
+      name: "departmentName",
+      type: "input",
+      message: "Enter the department name?"
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO department SET ?",
+      {
+        name: answer.departmentName
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("New department created!");
+        // re-prompt the user for if they want to bid or post
+        start();
+      }
+    );
+  });
+}
+
+// This function is used to create new roles for a specific department in the database. First you choose a department then you add a new role.
+function addRole() {
+  inquirer.prompt([
+    {
+      name: "department",
+      type: "list",
+      message: "Choose a department."
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      "INSERT INTO department SET ?",
+      {
+        name: answer.departmentName
+      },
+      function(err) {
+        if (err) throw err;
+        console.log("New department created!");
+        // re-prompt the user for if they want to bid or post
+        start();
+      }
+    );
   });
 }
--- seeds.sql
+++ seeds.sql
@@ -1,16 +1,20 @@
-USE employee_tracker_db
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 75000, 1);
-VALUES ("Supervisor", 50000, 2);
-VALUES ("Human Resource Representative", 70000, 3);
-VALUES ("Cashier", 36000, 4);
-VALUES ("Warehouse Worker", 38000, 5);
-VALUES ("Warehouse Manager", 70000, 6);
-VALUES ("Sales Associate", 40000, 7);
+USE employee_tracker_db;
 
 INSERT INTO department (name)
+VALUES ("HR"), ("Warehouse"), ("Front");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+("Manager", 75000, 1),
+("Manager", 75000, 2),
+("Manager", 75000, 3),
+("Supervisor", 50000, 2),
+("Human Resource Representative", 70000, 1),
+("Cashier", 36000, 3),
+("Worker", 38000, 2),
+("Warehouse Manager", 70000, 2),
+("Sales Associate", 40000, 3);
 
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("John", "Smith", 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith",1 ,null), ("mary", "Smith", 4,2),
